05e4fc81c3e9c5027a172cb38470786630ea8d6f
spocksbeerd <albueduard21@gmail.com>
2024-07-09 13:07:05 +0300

    routine changes

--- .local/bin/zoom.sh
+++ .local/bin/zoom.sh
@@ -4,28 +4,28 @@ get_zoom_factor() {
     hyprctl getoption cursor:zoom_factor | awk -F ":" '/float/ {print $2}'
 }
 
-SCALE=0.2
+ZOOM_STEP=0.2
 MAX_ZOOM=5.0
 MIN_ZOOM=1.0
 
 case $1 in
     in)
-        current_zoom="$(get_zoom_factor)" #String
-        new_zoom=`echo $current_zoom + $SCALE | bc -l` #String + float -> float
-        if (( $(echo "$new_zoom <= $MAX_ZOOM" | bc -l) )); then #compare float and float
+        current_zoom="$(get_zoom_factor)"
+        new_zoom=$(echo "$current_zoom + $ZOOM_STEP" | bc -l)
+        if (( $(echo "$new_zoom <= $MAX_ZOOM" | bc -l) )); then
             hyprctl keyword cursor:zoom_factor "$new_zoom"
         fi
 	;;
 
     out)
-        current_zoom="$(get_zoom_factor)" #String
-        new_zoom=`echo $current_zoom - $SCALE | bc -l` #String - float -> float
-        if (( $(echo "$new_zoom >= $MIN_ZOOM" | bc -l) )); then #compare float and float
+        current_zoom="$(get_zoom_factor)"
+        new_zoom=$(echo "$current_zoom - $ZOOM_STEP" | bc -l)
+        if (( $(echo "$new_zoom >= $MIN_ZOOM" | bc -l) )); then
             hyprctl keyword cursor:zoom_factor "$new_zoom"
         fi
 	;;
     
     reset)
-        hyprctl keyword cursor:zoom_factor 1
+        hyprctl keyword cursor:zoom_factor 1.0
     ;;
 esac
