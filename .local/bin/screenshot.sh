a95663dd6bf7dfdba84ed2192ddc74e4abf9f1c0
spocksbeerd <albueduard21@gmail.com>
2024-07-13 19:01:19 +0300

    routine changes

--- .local/bin/screenshot.sh
+++ .local/bin/screenshot.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-if ! pgrep -x "slurp" > /dev/null
-then
-    grim -t jpeg -g "$(slurp)" ~/pictures/screenshots/$(date +%Y-%m-%d_%H-%m-%s).jpg
-fi
+case "$1" in 
+    fullscreen)
+        grim -t jpeg ~/pictures/screenshots/$(date +%Y-%m-%d_%H-%m-%s).jpg
+    ;;
+
+    selection)
+        if ! pgrep -x "slurp" > /dev/null
+        then
+            grim -t jpeg -g "$(slurp)" ~/pictures/screenshots/$(date +%Y-%m-%d_%H-%m-%s).jpg
+        fi
+    ;;
+esac
+
+paplay "$HOME/.local/share/sfx/screenshot.mp3"
