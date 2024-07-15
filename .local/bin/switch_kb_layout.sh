84eec97f4dceddc3678c7a2f219ae8141bc1d212
spocksbeerd <albueduard21@gmail.com>
2024-07-15 18:54:04 +0300

    routine changes

--- .local/bin/switch_kb_layout.sh
+++ .local/bin/switch_kb_layout.sh
@@ -2,5 +2,5 @@
 
 kb_name=$(hyprctl devices | awk '/Keyboard at /{getline; keyboard=$0} /main: yes/{print keyboard; exit}' | awk '{print $1}')
 hyprctl switchxkblayout "$kb_name" next > /dev/null
-layout=$(hyprctl devices | awk "/${kb_name}/,/^Keyboard at /{if (\$0 ~ /active keymap:/) {sub(/^[^:]*: /, \"\", \$0); print; exit}}")
+layout=$(hyprctl devices | grep -B 1 "main: yes" | head -n 1 | grep -oP '(?<=active keymap: ).*')
 dunstify -u low -r 1000 "$layout" -t 1000
