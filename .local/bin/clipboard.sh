7efa5cd8b24d6117566c9dc2f0ed624c8f580466
spocksbeerd <albueduard21@gmail.com>
2024-07-15 19:29:22 +0300

    routine changes

--- .local/bin/clipboard.sh
+++ .local/bin/clipboard.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-cliphist list | rofi -dmenu -config "$XDG_CONFIG_HOME/rofi/config_for_clipboard.rasi" | cliphist decode | wl-copy
+case "$1" in 
+    show)
+        cliphist list | rofi -dmenu -config "$XDG_CONFIG_HOME/rofi/config_for_clipboard.rasi" | cliphist decode | wl-copy
+    ;;
+
+    clear)
+        cliphist wipe
+        rm "$XDG_CACHE_HOME/cliphist/db"
+        dunstify -u low "Clipboard cleared" -t 1000
+    ;;
+esac
