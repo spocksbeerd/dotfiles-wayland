2e1559ce2d506da1ae40dd14c7c998819e51e9b0
spocksbeerd <albueduard21@gmail.com>
2024-08-05 07:52:45 +0300

    routine changes

--- .local/bin/powermenu.sh
+++ .local/bin/powermenu.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-option=$(printf "  Restart\n  Shutdown\n󰒲  Suspend\n  Lock" | rofi -dmenu -config "$XDG_CONFIG_HOME/rofi/power_menu.rasi")
+option=$(printf "  Restart\n  Shutdown\n󰖔  Suspend\n󰍃  Logout\n  Lock" | rofi -dmenu -config "$XDG_CONFIG_HOME/rofi/power_menu.rasi")
 
 case "$option" in
 	"  Restart") reboot ;;
 	"  Shutdown") shutdown now ;;
-	"󰒲  Suspend") systemctl suspend ;;
+	"󰖔  Suspend") systemctl suspend ;;
+    "󰍃  Logout") pkill -KILL -u "$USER" ;;
 	"  Lock") lockscreen.sh ;;
 	*) exit 1 ;;
 esac
