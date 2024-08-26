c2285b800bcdd83f269f11b88c42c6092e6f4deb
spocksbeerd <albueduard21@gmail.com>
2024-08-26 06:37:29 +0300

    routine changes

--- .local/bin/node.sh
+++ .local/bin/node.sh
@@ -4,7 +4,4 @@ if [ -d "$HOME/.local/share/nvm/versions" ]; then
     dir=$HOME/.local/share/nvm/versions/node
     latest_version=$(ls "$dir" | tail -n 1)
     PATH="$PATH:$dir/$latest_version/bin/"
-else
-    echo "Node is not installed."
-    echo "You have to uncomment nvm in .zshrc to install it."
 fi
