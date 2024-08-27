e9add1d2c2050974e1b88e0a3df257ce30fafd87
spocksbeerd <albueduard21@gmail.com>
2024-08-27 15:35:02 +0300

    routine changes

--- .local/bin/wallpaper.sh
+++ .local/bin/wallpaper.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
 
-wallpaper=$(ls "$HOME/pictures/wallpapers" | rofi -dmenu -config "$XDG_CONFIG_HOME/rofi/generic_menu.rasi")
-
-if [ "$wallpaper" = "" ]; then
+usage() {
+    echo "Usage: wallpaper.sh [option]"
+    echo "Options:"
+    echo "  --restore      Apply the last set wallpaper"
+    echo "  --set          Set a new wallpaper"
+    echo "  --help         Show this menu"
     exit 1
-fi
+}
 
-pkill swaybg
-swaybg -i "$HOME/pictures/wallpapers/$wallpaper" &
-echo "\$wallpaper = ~/pictures/wallpapers/$wallpaper" > "$HOME/.config/hypr/wallpaper.conf"
+case "$1" in
+    --restore)
+        current_wallpaper=$(cat "$HOME/.cache/wallpaper")
+        swaybg -i "$HOME/pictures/wallpapers/$current_wallpaper" &
+        ;;
+    --set)
+        wallpaper=$(ls "$HOME/pictures/wallpapers" | rofi -dmenu -config "$XDG_CONFIG_HOME/rofi/generic_menu.rasi")
+
+        if [ "$wallpaper" = "" ]; then
+            exit 1
+        fi
+
+        pkill swaybg
+        swaybg -i "$HOME/pictures/wallpapers/$wallpaper" &
+        echo "$wallpaper" > "$HOME/.cache/wallpaper"
+        ;;
+    --help)
+        usage
+        ;;
+    *)
+        echo "Invalid option: \"$1\"" >&2
+        usage
+        ;;
+esac
