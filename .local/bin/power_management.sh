b8fa83e8cbaf276f9bf42591d2250d1d657f1709
spocksbeerd <albueduard21@gmail.com>
2024-09-01 04:47:13 +0300

    routine changes

---
+++ .local/bin/power_management.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+swayidle -w \
+    timeout 300 "lockscreen.sh && hyprctl dispatch dpms off" \
+    timeout 600 "systemctl suspend" \
+    before-sleep "lockscreen.sh" \
+    after-resume "hyprctl dispatch dpms on"
