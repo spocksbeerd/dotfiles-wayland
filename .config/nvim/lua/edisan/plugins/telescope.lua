27ca7f2d260e9ccc952dbf7725f275371f2ba123
spocksbeerd <albueduard21@gmail.com>
2024-10-17 22:55:27 +0300

    routine changes

--- .config/nvim/lua/edisan/plugins/telescope.lua
+++ .config/nvim/lua/edisan/plugins/telescope.lua
@@ -1,3 +1,9 @@
+require('telescope').setup {
+  defaults = {
+    history = false,
+  },
+}
+
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
