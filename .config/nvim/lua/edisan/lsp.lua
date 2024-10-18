08f035db129721df8a6b1e4077630aca0b00b520
spocksbeerd <albueduard21@gmail.com>
2024-10-19 01:23:53 +0300

    lsp icons

--- .config/nvim/lua/edisan/lsp.lua
+++ .config/nvim/lua/edisan/lsp.lua
@@ -7,81 +7,77 @@ require("mason-lspconfig").setup {
     automatic_installation = true,
 }
 
-require("lspconfig").lua_ls.setup {
+local lspconfig = require("lspconfig")
+lspconfig.lua_ls.setup {
     settings = {
         Lua = {
             diagnostics = {
-                -- Get the language server to recognize the `vim` global
                 globals = { "vim" },
             },
         },
     },
 }
-require("lspconfig").clangd.setup {}
-require("lspconfig").gopls.setup {}
-require("lspconfig").ts_ls.setup {}
-require("lspconfig").emmet_language_server.setup {}
-require("lspconfig").eslint.setup {}
--- require("lspconfig").omnisharp.setup {}
--- require("lspconfig").jdtls.setup {}
--- require("lspconfig").pylsp.setup {}
+lspconfig.clangd.setup {}
+lspconfig.gopls.setup {}
+lspconfig.ts_ls.setup {}
+lspconfig.emmet_language_server.setup {}
+lspconfig.eslint.setup {}
+-- lspconfig.omnisharp.setup {}
+-- lspconfig.jdtls.setup {}
+-- lspconfig.pylsp.setup {}
 
 vim.api.nvim_create_autocmd("LspAttach", {
     group = vim.api.nvim_create_augroup("UserLspConfig", {}),
     callback = function(ev)
-        -- Enable completion triggered by <c-x><c-o>
-        vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
-
-        -- Buffer local mappings.
-        -- See `:help vim.lsp.*` for documentation on any of the below functions
         local opts = { buffer = ev.buf }
-        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
         vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
         vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-        vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
         vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-        vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
-        vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
-        vim.keymap.set("n", "<leader>wl", function()
-            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        end, opts)
         vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
+        vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
         vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
         vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-        vim.keymap.set("n", "<leader>f", function()
-            vim.lsp.buf.format { async = true }
-        end, opts)
+        vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
+        vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
+        vim.keymap.set("n", "<leader>wl", function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, opts)
+        vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format { async = true } end, opts)
     end,
 })
 
--- Better visibility for diagnostics
-local _border = "rounded"
+local border = "rounded"
 
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
     vim.lsp.handlers.hover, {
-        border = _border
+        border = border
     }
 )
-
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
     vim.lsp.handlers.signature_help, {
-        border = _border
+        border = border
     }
 )
-
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics, {
-        -- delay update diagnostics
         update_in_insert = false,
     }
 )
-
 vim.diagnostic.config({
-    virtual_text = true,
-    float = { border = _border }
+    virtual_text = {
+        prefix = "⚑", -- "▎", "■", "x", "●"
+    },
+    float = {
+        border = border,
+    }
 })
+
+local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
+for type, icon in pairs(signs) do
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
 
 -- Show line diagnostics automatically when hovering
 -- vim.o.updatetime = 250
