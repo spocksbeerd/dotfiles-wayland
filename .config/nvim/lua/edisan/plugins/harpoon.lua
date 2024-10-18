8d74478eed3fa903172e3fce047467c4dbed7ccc
spocksbeerd <albueduard21@gmail.com>
2024-10-18 07:00:10 +0300

    routine changes

--- .config/nvim/lua/edisan/plugins/harpoon.lua
+++ .config/nvim/lua/edisan/plugins/harpoon.lua
@@ -1,10 +1,15 @@
 local harpoon = require("harpoon")
 
-harpoon:setup()
+local toggle_opts = {
+    title = " Harpoon ",
+    title_pos = "center",
+    border = "rounded",
+    ui_width_ratio = 0.40,
+}
 
 vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-vim.keymap.set("n", "<leader><Tab>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<C-2>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<C-3>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<C-4>", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<leader><Tab>", function() harpoon.ui:toggle_quick_menu(harpoon:list(), toggle_opts) end)
+vim.keymap.set("n", "<A-1>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<A-2>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<A-3>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<A-4>", function() harpoon:list():select(4) end)
