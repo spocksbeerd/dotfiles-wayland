cd5f5ca2c37ae38c0ab29adf618e309af237c9a8
spocksbeerd <albueduard21@gmail.com>
2024-10-25 07:37:41 +0300

    remove unneeded shell script

--- .local/bin/node.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ -d "$HOME/.local/share/nvm/versions" ]; then
-    dir=$HOME/.local/share/nvm/versions/node
-    latest_version=$(ls "$dir" | tail -n 1)
-    PATH="$PATH:$dir/$latest_version/bin/"
-fi
