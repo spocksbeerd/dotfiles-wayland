0dc1da2210671c829251a340bc3d186a2fd848d0
spocksbeerd <albueduard21@gmail.com>
2024-11-16 21:28:04 +0200

    feat: add support for hypridle and hyprlock

--- .local/bin/power_management.sh
+++ .local/bin/power_management.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-swayidle -w \
-    timeout 300 "lockscreen.sh && hyprctl dispatch dpms off" \
-    timeout 600 "systemctl suspend" \
-    before-sleep "lockscreen.sh" \
-    after-resume "hyprctl dispatch dpms on" &
+idle_daemon=hypridle
+
+killall swayidle
+killall hypridle
+
+if [ "$idle_daemon" = "swayidle" ]; then
+    swayidle -w \
+        timeout 300 "lockscreen.sh && hyprctl dispatch dpms off" \
+        timeout 600 "systemctl suspend" \
+        before-sleep "lockscreen.sh" \
+        after-resume "hyprctl dispatch dpms on" &
+else
+    hypridle
+fi
