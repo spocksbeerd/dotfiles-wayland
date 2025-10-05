321fd3caad5f24bd94a4405eae828ff61042e60e
spocksbeerd <albueduard21@gmail.com>
2025-10-05 12:49:07 +0300

    very important change

--- .local/bin/brightness.sh
+++ .local/bin/brightness.sh
@@ -2,14 +2,14 @@
 
 current_brightness=$(brightnessctl get)
 
-if [ "$current_brightness" -le 230 ]; then
+if [ "$current_brightness" -le 63 ]; then
     brightnessctl set 25%
-elif [ "$current_brightness" -le 465 ]; then
+elif [ "$current_brightness" -le 127 ]; then
     brightnessctl set 50%
-elif [ "$current_brightness" -le 700 ]; then
+elif [ "$current_brightness" -le 190 ]; then
     brightnessctl set 75%
-elif [ "$current_brightness" -le 930 ]; then
+elif [ "$current_brightness" -le 230 ]; then
     brightnessctl set 100%
-elif [ "$current_brightness" -le 1000 ]; then
+elif [ "$current_brightness" -le 255 ]; then
     brightnessctl set 5%
 fi
