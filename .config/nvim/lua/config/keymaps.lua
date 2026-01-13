e0ac801734c36fdbb58bdf464623be68dd876f3b
spocksbeerd <albueduard21@gmail.com>
2026-01-13 03:13:09 +0200

    very important change

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -6,4 +6,9 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 keymap("i", "jk", "<ESC>", opts)
-keymap("n", "<leader>e", ":Ex<cr>", opts)
+-- keymap("n", "<leader>e", ":Ex<cr>", opts)
+
+vim.keymap.set('n', '<C-Up>',    ':resize +1<CR>', opts)
+vim.keymap.set('n', '<C-Down>',  ':resize -1<CR>', opts)
+vim.keymap.set('n', '<C-Left>',  ':vertical resize -2<CR>', opts)
+vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>', opts)
