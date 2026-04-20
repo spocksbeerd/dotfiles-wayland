cacf4273933cc1b6face2474bbfe5363f27411eb
spocksbeerd <albueduard21@gmail.com>
2026-04-20 13:02:08 +0300

    very important change

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,17 +1,15 @@
 local opts = { noremap = true, silent = true }
 
-local keymap = vim.api.nvim_set_keymap
-
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-keymap("i", "jk", "<ESC>", opts)
-keymap("n", "<leader>e", ":Ex<cr>", opts)
+vim.keymap.set("i", "jk", "<ESC>", opts)
+vim.keymap.set("n", "<leader>e", ":Ex<cr>", opts)
 
-vim.keymap.set('n', '<C-Up>',    ':resize +1<CR>', opts)
-vim.keymap.set('n', '<C-Down>',  ':resize -1<CR>', opts)
-vim.keymap.set('n', '<C-Left>',  ':vertical resize -2<CR>', opts)
-vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>', opts)
+vim.keymap.set("n", "<C-Up>",    ":resize +1<CR>", opts)
+vim.keymap.set("n", "<C-Down>",  ":resize -1<CR>", opts)
+vim.keymap.set("n", "<C-Left>",  ":vertical resize -2<CR>", opts)
+vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
-vim.keymap.set('x', 'p', '"_dP', { noremap = true })
-vim.keymap.set('x', 'P', '"_dP', { noremap = true })
+vim.keymap.set("x", "p", '"_dP', opts)
+vim.keymap.set("x", "P", '"_dP', opts)
